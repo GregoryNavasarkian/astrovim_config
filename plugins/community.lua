2055028132522512cdbd13f99c81a4d9520e2990
Gregory <gregory@pop-os.localdomain>
2023-11-10 03:16:40 -0800

    add plugins and theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,21 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python"},
+  { import = "astrocommunity.pack.tailwindcss"},
+  { import = "astrocommunity.pack.typescript"},
+  { import = "astrocommunity.pack.rust"},
+  { import = "astrocommunity.pack.json"},
+  { import = "astrocommunity.pack.cpp"},
+  { import = "astrocommunity.pack.cs"},
+  { import = "astrocommunity.pack.docker"},
+  { import = "astrocommunity.pack.bash"},
+  { import = "astrocommunity.pack.go"},
+  { import = "astrocommunity.pack.markdown"},
+  { import = "astrocommunity.pack.html-css"},
+  { import = "astrocommunity.pack.prisma"},
+  { import = "astrocommunity.pack.cmake"},
+  { import = "astrocommunity.pack.angular"},
 }
