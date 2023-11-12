b5a72d4a4359548ec32e4d44539d36c9868eefe8
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-11-11 22:23:42 -0800

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,16 +9,27 @@ return {
   { import = "astrocommunity.pack.python"},
   { import = "astrocommunity.pack.tailwindcss"},
   { import = "astrocommunity.pack.typescript"},
-  --{ import = "astrocommunity.pack.rust"},
+  { import = "astrocommunity.pack.rust"},
   { import = "astrocommunity.pack.json"},
-  { import = "astrocommunity.pack.cpp"},
-  --{ import = "astrocommunity.pack.cs"},
-  --{ import = "astrocommunity.pack.docker"},
-  { import = "astrocommunity.pack.bash"},
-  --{ import = "astrocommunity.pack.go"},
+  --{ import = "astrocommunity.pack.cpp"},
+  { import = "astrocommunity.pack.cs"},
+  { import = "astrocommunity.pack.docker"},
+  --{ import = "astrocommunity.pack.bash"},
+  { import = "astrocommunity.pack.go"},
   { import = "astrocommunity.pack.markdown"},
   { import = "astrocommunity.pack.html-css"},
-  --{ import = "astrocommunity.pack.prisma"},
-  { import = "astrocommunity.pack.cmake"},
+  { import = "astrocommunity.pack.prisma"},
+  --{ import = "astrocommunity.pack.cmake"},
   { import = "astrocommunity.pack.angular"},
+  { import = "astrocommunity.bars-and-lines.lualine-nvim"}, --bottom line
+  { import = "astrocommunity.editing-support.multicursors-nvim"}, -- multi cursor "lead m" or cmd"MCstart"
+  { import = "astrocommunity.editing-support.neogen"}, --add documentation before funcs lead a
+  { import = "astrocommunity.indent.indent-blankline-nvim"},
+  { import = "astrocommunity.motion.nvim-spider"},
+  { import = "astrocommunity.motion.harpoon"}, -- lead+lead
+  { import = "astrocommunity.motion.nvim-surround"}, -- norm mode use 's' visual use 'S' t=html tag :h nvim-surround
+  { import = "astrocommunity.scrolling.nvim-scrollbar"},
+  { import = "astrocommunity.scrolling.vim-smoothie"},
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank"},
+
 }
